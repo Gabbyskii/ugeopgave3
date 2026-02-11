dd778873bf31331e1062c0aad9af886a093a7909
Gabbyskii <gkadiga1@gmail.com>
2026-02-11 17:14:40 +0100

    opg1 bank done

---
+++ src/bankKonto/Main.java
@@ -0,0 +1,42 @@
+package bankKonto;
+
+import java.util.Scanner;
+
+public class Main {
+    void main(){
+        accountName = "Gabby";
+        //deposit100();
+
+        // udfordring:
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("How much do you want to deposit? ");
+        double money = sc.nextDouble();
+        deposit(money);
+        System.out.println(money + " deposited to " + accountName +"'s account.");
+        withdraw(25);
+        printBalance();
+
+
+    }
+
+    double balance;
+    String accountName;
+
+    void deposit(double amount){
+        balance += amount;
+    }
+
+    void withdraw(double amount){
+        balance -= amount;
+    }
+
+    void printBalance(){
+        System.out.println("Konto name: " + accountName + "\nAccount balance: "+ balance);
+    }
+
+
+
+
+
+}
